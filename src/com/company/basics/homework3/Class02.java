4113239659e4f8de8784b684a83e8250d2b1c0ad
RuslanDziub <dziub8888ruslan@gmail.com>
2021-03-03 13:12:29 +0200

    homework3.2

---
+++ src/com/company/basics/homework3/Class02.java
@@ -0,0 +1,12 @@
+package com.company.basics.homework3;
+
+public class Class02 {
+    public static void main(String[] args) {
+        int element = 1;
+
+        for (int i = 1; i <= 55; i++) {
+            System.out.println("#" + i + " = " + element);
+            element += 2;
+        }
+    }
+}
